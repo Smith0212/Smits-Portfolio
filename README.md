d4cac4b1dd859b98bf63dfa5382181f60ff7001e
Smit0212 <smitsutariya056+GitHub@gmail.com>
2023-12-31 11:29:44 +0530

    added portfolio code

---
+++ README.md
@@ -0,0 +1,25 @@
+# Responsive Portfolio Website
+
+This project is a responsive portfolio website developed by Aditya Jethani. It is designed using HTML, CSS, and JavaScript, featuring:
+
+- **Responsive Design:** Built to be responsive on all devices - mobile, tablet, and desktop.
+- **Scroll Animations:** Includes animations while scrolling to enhance user experience.
+- **Smooth Scrolling:** Provides a smooth scrolling experience to navigate through different sections.
+- **Dark Theme:** Offers a beautiful dark theme for the website.
+- **Customizable Colors:** Allows customization of the project colors.
+- **Contact Section:** Includes a contact section with the functionality to send emails.
+- **Mobile-First Development:** Initially developed for mobile devices and extended for desktop compatibility.
+- **Cross-Device Compatibility:** Compatible with all mobile devices, providing a pleasant and intuitive user interface.
+
+## How to Use
+
+To use this project:
+1. Clone the repository.
+2. Open the project in your preferred code editor.
+3. Customize colors, content, and sections as needed.
+4. Deploy the website on your preferred hosting platform.
+
+## Credits
+
+- **Smit Sutariya:** Lead Developer
+
---
+++ script.js
@@ -0,0 +1,96 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 130);
+});
+
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".nav");
+
+menu.onclick = () => {
+    menu.classList.toggle("bx-x");
+    nav.classList.toggle("open");
+};
+
+window.onscroll = () => {
+    menu.classList.remove("bx-x");
+    nav.classList.remove("open");
+}; 
+
+// Type JS
+document.addEventListener('DOMContentLoaded', function() {
+    const typed = new Typed('.multiple-text', {
+        strings: ["Web Developer!", "Web Scraper!", "Python Programmer!!"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    });
+});
+
+
+// function sendEmail() {
+//     Email.send({
+//         Host : "smtp.gmail.com",
+//         Username : "smitkumar.sce21@sot.pdpu.ac.in",
+//         Password : "**********",
+//         To : 'smitsutariya056@gmail.com',
+//         From : document.getElementById("email").value,
+//         Subject : document.getElementById("subject").value,
+//         Body : "Name: " + document.getElementById("yourname").value +
+//                "<br> Message: " + document.getElementById("text").value
+//     }).then(
+//       message => alert("Mail Sent Succfesfully")
+//     );
+// }
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+        contactMessage = document.getElementById('text')
+const sendEmail = (e) =>{
+    e.preventDefault()
+    //service id, template id, template params
+    emailjs.sendForm('service_rujmvlk','template_0v7dudm',"#contact-form",'qtRH3dBS67TzrxnS-')
+    .then(() => {
+        contactMessage.textContent='✅ Message sent successfully'
+        //rremove the message after 5 seconds
+        setTimeout(() => {
+            contactMessage.textContent=''
+        },5000)
+
+        //reset the form
+        contactForm.reset()
+
+    }, () => {
+        contactMessage.textContent='❌ Message not sent (server error)'
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
+
+
+function validateForm() {
+    // Get the email input element
+    var emailInput = document.forms["contact-form"]["email"];
+
+    // Get the value of the email input
+    var email = emailInput.value;
+
+    // Regular expression for email validation
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    // Check if the email is valid
+    if (!emailPattern.test(email)) {
+       // If the email is not valid, show an error message
+       var errorMessage = "Please enter a valid email address.";
+       var contactMessage = document.getElementById("text");
+       contactMessage.innerHTML = errorMessage;
+       contactMessage.style.color = "red";
+       return false; // Prevent form submission
+    }
+
+    // If the email is valid, clear any previous error messages (if any)
+    var contactMessage = document.getElementById("text");
+    contactMessage.innerHTML = "";
+
+    // Allow form submission
+    return true;
+ }
